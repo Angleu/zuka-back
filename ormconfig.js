579eb82d0e64e4c42e668223541906b09d627eb5
Angleu <angleuzua99@gmail.com>
2022-01-13 18:02:17 +0000

    make it better

--- ormconfig.js
+++ ormconfig.js
@@ -2,22 +2,22 @@ console.log(process.env.DATABASE_URL);
 module.exports = {
    "type": "postgres",
    "url": process.env.DATABASE_URL,
-   // "host": "ec2-18-234-17-166.compute-1.amazonaws.com",
-   // "port": 5432,
-   // "username": "lrlpgtfoisugwb",
-   // "password": "98db7f1db7348866443395dd61e2521a62b0bd35517d9365d2fb4aff70f09494",
-   // "database": "d8f08eeicmbh8n",
+   "host": "ec2-18-234-17-166.compute-1.amazonaws.com",
+   "port": 5432,
+   "username": "lrlpgtfoisugwb",
+   "password": "98db7f1db7348866443395dd61e2521a62b0bd35517d9365d2fb4aff70f09494",
+   "database": "d8f08eeicmbh8n",
    "entities": [
-      "src/model/**/*.ts"
+      "dist/model/**/*.ts"
    ],
    "migrations": [
-      "src/migration/**/*.ts"
+      "dist/migration/**/*.ts"
    ],
-   // "synchronize":false,
-   // "ssl":{
-   //    "require": true,
-   //    "rejectUnauthorized": false,
-   // },
+   "synchronize":false,
+   "ssl":{
+      "require": true,
+      "rejectUnauthorized": false,
+   },
    "cli": {
       "entitiesDir": "src/model",
       "migrationsDir": "src/migration",
