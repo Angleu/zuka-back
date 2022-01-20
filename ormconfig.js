741773808b6a6e85d94298d85fb6ef86028fb9f8
Angleu <angleuzua99@gmail.com>
2022-01-20 23:26:24 +0000

    fix

--- ormconfig.js
+++ ormconfig.js
@@ -1,28 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 console.log(process.env.DATABASE_URL);
 module.exports = {
-   "type": "postgres",
-   "url": process.env.DATABASE_URL,
-   // "host": "ec2-18-234-17-166.compute-1.amazonaws.com",
-   // "port": 5432,
-   // "username": "lrlpgtfoisugwb",
-   // "password": "98db7f1db7348866443395dd61e2521a62b0bd35517d9365d2fb4aff70f09494",
-   // "database": "d8f08eeicmbh8n",
-   logging: true,
-   synchronize: true,
-   entities: [
-       "dist/model/*.js"
-   ],
-   subscribers: [
-       "dist/subscriber/*.js"
-   ],
-   migrations: [
-       "dist/migration/*.js"
-   ],
-   cli: {
-       entitiesDir: "src/model",
-       migrationsDir: "src/migration",
-       subscribersDir: "src/subscriber"
-   }
-}
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  // "host": "ec2-18-234-17-166.compute-1.amazonaws.com",
+  // "port": 5432,
+  // "username": "lrlpgtfoisugwb",
+  // "password": "98db7f1db7348866443395dd61e2521a62b0bd35517d9365d2fb4aff70f09494",
+  // "database": "d8f08eeicmbh8n",
+  logging: true,
+  logging: true,
+  ssl: {
+    ca: process.env.SSL_CERT,
+  },
+  synchronize: true,
+  entities: ["dist/model/*.js"],
+  subscribers: ["dist/subscriber/*.js"],
+  migrations: ["dist/migration/*.js"],
+  cli: {
+    entitiesDir: "src/model",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+};
