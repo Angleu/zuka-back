6df3adcbc4f79747c0086b8beeed660795fcace8
Angleu <angleuzua99@gmail.com>
2022-02-15 00:58:56 +0000

    fix transation

--- dist/routes/transation.routes.js
+++ dist/routes/transation.routes.js
@@ -7,5 +7,5 @@ var express_1 = require("express");
 var TransationController_1 = __importDefault(require("../Controller/TransationController"));
 var routes = (0, express_1.Router)();
 routes.get('/account/transation/:id_account', new TransationController_1.default().handleExecute);
-routes.post('/account/transation', new TransationController_1.default().handleSave);
+routes.post('/account/transation', new TransationController_1.default().handleSave); // post
 exports.default = routes;
--- src/routes/transation.routes.ts
+++ src/routes/transation.routes.ts
@@ -5,7 +5,7 @@ import TransationController from '../Controller/TransationController';
 const routes = Router();
 
 routes.get('/account/transation/:id_account', new TransationController().handleExecute);
-routes.post('/account/transation', new TransationController().handleSave);
+routes.post('/account/transation', new TransationController().handleSave); // post
 
 
 export default routes;
